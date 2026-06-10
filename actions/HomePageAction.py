2bdca905d3980d5b7d3a4a6b552b4c846fe55007
TAMILKUMAR0027 <tamilkumar0027@gmail.com>
2026-06-10 19:50:01 +0530

    Created the base action page and base page

---
+++ actions/HomePageAction.py
@@ -0,0 +1,14 @@
+from actions.BaseAction import BaseAction
+from pages.HomePage import HomePage
+
+
+class HomePageAction(BaseAction):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.hp = HomePage()
+
+    def clickShopByCategory(self):
+        self.click(self.hp.ShopByCategory)
+    def clickMonitor(self):
+        self.click(self.hp.Monitor)

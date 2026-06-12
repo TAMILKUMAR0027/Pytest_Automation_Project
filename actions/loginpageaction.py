c94354cab3789abca76c95d7c2369011277cbddf
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-06-12 14:03:36 +0530

    Added invalid login and register

--- actions/loginpageaction.py
+++ actions/loginpageaction.py
@@ -18,3 +18,10 @@ class LoginPageAction(BaseAction):
         self.send_keys(self.lp.loginEmail, lemail)
         self.send_keys(self.lp.loginPassword, lpass)
         self.click(self.lp.loginContinue)
+
+    def provide_error_msg_invalid_login(self):
+        actual = self.get_text(self.lp.invalidLoginErrorMsg)
+        actual = " ".join(actual.split())
+        invalid_msg = "Warning: No match for E-Mail Address and/or Password."
+        attempt_exceeded_msg = " Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour."
+        return invalid_msg in actual or attempt_exceeded_msg in actual
--- pages/loginpage.py
+++ pages/loginpage.py
@@ -10,3 +10,7 @@ class LoginPage(BasePage):
     loginEmail = (By.XPATH, "//input[@name='email']")
     loginPassword = (By.XPATH, "//input[@name='password']")
     loginContinue = (By.XPATH, "//input[@type='submit']")
+    invalidLoginErrorMsg = (
+        By.XPATH,
+        "//div[@id='account-login']/child::div[text()=' Warning: No match for E-Mail Address and/or Password.']",
+    )

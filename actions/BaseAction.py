4283acb8b38d637a962b10fd4b10dff2f1bf1aee
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-06-12 11:06:22 +0530

    Remove unnecessary character from BaseAction.py

--- actions/BaseAction.py
+++ actions/BaseAction.py
@@ -2,7 +2,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-qw
 
 class BaseAction:
 

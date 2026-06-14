60467cf92274e4a74e4dafbe46051873ed866fe6
Jothika <raviravi31799@gmail.com>
2026-06-14 18:08:55 +0530

    Added invalid review with rating only

--- tests/test_addreview.py
+++ tests/test_addreview.py
@@ -3,9 +3,9 @@ from utils.configReader import ConfigReader
 from actions.HomePageAction import HomePageAction
 from actions.AddReviewpageaction import AddReviewpageaction
 from utils.excelReader import get_data
-from utils.loggerCreator import get_logger
+from utils import loggerCreator
 
-logger = get_logger(__name__)
+logger = loggerCreator.get_logger(__name__)
 
 
 @pytest.mark.Jothika
@@ -25,9 +25,43 @@ class TestAddreview:
         url=ConfigReader.get_url()
         logger.info("Application launch is successful")
         self.arpa.selectproduct()
+        logger.info("Selected the product")
         self.arpa.moveto_review()
+        logger.info("Found review page")
         self.arpa.selectrating(rating)
+        logger.info("Selected Rating")
         self.arpa.enterName(name)
+        logger.info("Entered reviewer name")
         self.arpa.enterfeedback(feedback)
+        logger.info("Entered feedback")
         self.arpa.clicksubmit()
+        logger.info("Submitted review")
+        self.arpa.successmsg(expectedMessage)
+        logger.info("Review added successfully")
+
+    @pytest.mark.parametrize(
+        "rating,expectedMessage",
+        get_data(
+            "data_provider/DataProvider.xlsx",
+            "AddReview-rating",
+        ),
+    )
+
+    def test_withrating(self,driver,rating,expectedMessage):
+          drv, wait = driver
+          self.arpa=AddReviewpageaction(drv)
+          url=ConfigReader.get_url()
+          logger.info("Application launch is successful")
+          self.arpa.selectproduct()
+          logger.info("Selected the product")
+          self.arpa.moveto_review()
+          logger.info("Found review page")
+          self.arpa.selectrating(rating)
+          logger.info("Selected Rating")
+          self.arpa.clicksubmit()
+          logger.info("Submitted review")
+          self.arpa.warningmsg(expectedMessage)
+          logger.info("Invalid review submitted successfully")
+
+
 
